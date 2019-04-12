2fa8fb4964a5c317cf1b804d5c80a74c48637acf
MathiasReker <mathias@reker.dk>
2019-04-12 17:48:05 +0200

    Function declaration

--- src/Pakkelabels.php
+++ src/Pakkelabels.php
@@ -7,108 +7,108 @@ class Pakkelabels {
   private $_api_user;
   private $_api_key;
 
-  public function __construct($api_user, $api_key, $api_base_path=self::API_ENDPOINT){
+  public function __construct($api_user, $api_key, $api_base_path=self::API_ENDPOINT) {
     $this->_api_user = $api_user;
     $this->_api_key = $api_key;
     $this->_api_base_path = $api_base_path;
   }
 
-  public function account_balance(){
+  public function account_balance() {
     $result = $this->_make_api_call('/account/balance');
     return $result;
   }
 
-  public function account_payment_requests($params){
+  public function account_payment_requests($params) {
     $result = $this->_make_api_call('/account/payment_requests', 'GET', $params);
     return $result;
   }
 
-  public function products($params){
+  public function products($params) {
     $result = $this->_make_api_call('/products', 'GET', $params);
     return $result;
   }
 
-  public function pickup_points($params){
+  public function pickup_points($params) {
     $result = $this->_make_api_call('/pickup_points', 'GET', $params);
     return $result;
   }
 
-  public function shipment_monitor_statuses($params){
+  public function shipment_monitor_statuses($params) {
     $result = $this->_make_api_call('/shipment_monitor_statuses', 'GET', $params);
     return $result;
   }  
 
-  public function return_portals($params){
+  public function return_portals($params) {
     $result = $this->_make_api_call('/return_portals', 'GET', $params);
     return $result;
   }  
   
-  public function return_portal($id){
+  public function return_portal($id) {
     $result = $this->_make_api_call('/return_portals/' . $id);
     return $result;
   }
 
-  public function return_portal_shipments($return_portal_id, $params){
+  public function return_portal_shipments($return_portal_id, $params) {
     $result = $this->_make_api_call('/return_portals/' . $return_portal_id . '/shipments');
     return $result;
   }
 
-  public function shipments($params){
+  public function shipments($params) {
     $result = $this->_make_api_call('/shipments', 'GET', $params);
     return $result;
   }
 
-  public function shipment($id){
+  public function shipment($id) {
     $result = $this->_make_api_call('/shipments/' . $id);
     return $result;
   }
 
-  public function shipment_labels($id, $params){
+  public function shipment_labels($id, $params) {
     $result = $this->_make_api_call('/shipments/' . $id . '/labels', 'GET', $params);
     return $result;
   }
 
-  public function create_shipment($params){
+  public function create_shipment($params) {
     $result = $this->_make_api_call('/shipments', 'POST', $params);
     return $result;
   }
 
-  public function print_queue_entries($params){
+  public function print_queue_entries($params) {
     $result = $this->_make_api_call('/print_queue_entries', 'GET', $params);
     return $result;
   }  
 
-  public function imported_shipments($params){
+  public function imported_shipments($params) {
     $result = $this->_make_api_call('/imported_shipments', 'GET', $params);
     return $result;
   }
 
-  public function imported_shipment($id){
+  public function imported_shipment($id) {
     $result = $this->_make_api_call('/imported_shipments/' . $id);
     return $result;
   }
 
-  public function create_imported_shipment($params){
+  public function create_imported_shipment($params) {
     $result = $this->_make_api_call('/imported_shipments', 'POST', $params);
     return $result;
   }
 
-  public function update_imported_shipment($id, $params){
+  public function update_imported_shipment($id, $params) {
     $result = $this->_make_api_call('/imported_shipments/'. $id, 'PUT', $params);
     return $result;
   }
 
-  public function delete_imported_shipment($id){
+  public function delete_imported_shipment($id) {
     $result = $this->_make_api_call('/imported_shipments/'. $id, 'DELETE');
     return $result;
   }
 
-  public function labels($params){
+  public function labels($params) {
     $result = $this->_make_api_call('/labels/', 'GET', $params);
     return $result;
   }
   
-  private function _make_api_call($path, $method = 'GET',$params = array()){
+  private function _make_api_call($path, $method = 'GET',$params = array()) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_USERPWD, $this->_api_user . ":" . $this->_api_key);
     $params['user_agent'] = 'pdk_php_library v' . self::VERSION;
@@ -150,7 +150,7 @@ class Pakkelabels {
 
     // this function is called by curl for each header received
     curl_setopt($ch, CURLOPT_HEADERFUNCTION,
-      function($curl, $header) use (&$headers)
+      function ($curl, $header) use (&$headers)
       {
         $len = strlen($header);
         $header = explode(':', $header, 2);
--- src/PakkelabelsException.php
+++ src/PakkelabelsException.php
@@ -1,7 +1,7 @@
 <?php
 class PakkelabelsException extends Exception
 {
-	public function __construct($message){
+	public function __construct($message) {
 		parent::__construct($message);
 	}
 }
